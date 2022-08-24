e9415deece19a6691c3c3cfc14f229805357fd3d
aryanprince <aryanprincesanty@icloud.com>
2022-08-25 01:02:52 +0530

    Updated title and favicon

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
         <div className="flex h-screen justify-center items-center bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 background-animate">
             <div className="p-10 m-10 bg-white bg-opacity-20 backdrop-blur-lg rounded-lg drop-shadow-xl w-[40rem] font-sans font-medium text-lg">
                 <div className="flex items-center pb-4">
-                    <GoLightBulb className="mr-1" />
+                    <GoLightBulb className="mr-2" />
                     <p className=" text-[#242424] text-md">Random Advice</p>
                 </div>
                 <p className="text-[#242424] text-2xl">{advice.advice}</p>
